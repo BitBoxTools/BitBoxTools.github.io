b973026ea2ae449d28ab1ce9a7a7f8be99cd5402
wxt669698 <1970550145@qq.com>
2023-08-02 09:05:47 +0800

    update

--- assets/getEthPrice-4d29be13.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./axios-a9a89d3f.js";const o=async()=>{try{return(await e.post("https://mainnet.era.zksync.io/",{id:42,jsonrpc:"2.0",method:"zks_getTokenPrice",params:["0x0000000000000000000000000000000000000000"]})).data.result}catch(t){return console.log(t),0}};export{o as g};
