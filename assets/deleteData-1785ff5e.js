96f320acc6d30f88645bfc8ab80c323dec3e3884
wxt669698 <1970550145@qq.com>
2023-08-13 22:22:15 +0800

    update

---
+++ assets/deleteData-1785ff5e.js
@@ -0,0 +1,1 @@
+import{B as c,F as e,ab as n,ac as l}from"./index-d8440a21.js";const m=async(o,i)=>{try{await c(e);const a=i.map(async s=>{const t=await n(o,s);console.log(t),t&&await l(o,s)});await Promise.all(a)}catch(a){console.log(a)}};export{m as d};
--- assets/deleteData-c82fd0ef.js
+++
@@ -1,1 +0,0 @@
-import{G as i,H as c,ad as n,ae as l}from"./index-f03646fd.js";const m=async(o,e)=>{try{await i(c);const a=e.map(async s=>{const t=await n(o,s);console.log(t),t&&await l(o,s)});await Promise.all(a)}catch(a){console.log(a)}};export{m as d};
